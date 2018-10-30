a2f0393ab311ce10fb7ee36ee887dbec4ccf689a
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2018-10-30 09:34:39 -0500

    Completed the header and footer template, bar mouseover text.

---
+++ footer.php
@@ -0,0 +1,58 @@
+
+<footer>
+<div class="card text-center footer">
+  <div class="card-header">
+  <ul class="nav nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="http://blogs.rrc.ca/informationsystems" title="The ACE Program">
+                        <img class="navbar-nav-svg" src="./assets/img/logos/rrc.png" alt="The Applied Computer Education Program">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://github.com/bits-and-bytes-association/rrc-bba-website/graphs/contributors">
+                        <svg class="navbar-nav-svg" viewBox="0 0 16 16" aria-hidden="true">
+                            <title>Website Contributors</title>
+                            <path fill-rule="evenodd" d="M16 12.999c0 .439-.45 1-1 1H7.995c-.539 0-.994-.447-.995-.999H1c-.54 0-1-.561-1-1 0-2.634 3-4 3-4s.229-.409 0-1c-.841-.621-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.442.58 2.5 3c.058 2.41-.159 2.379-1 3-.229.59 0 1 0 1s1.549.711 2.42 2.088C9.196 9.369 10 8.999 10 8.999s.229-.409 0-1c-.841-.62-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.437.581 2.495 3c.059 2.41-.158 2.38-1 3-.229.59 0 1 0 1s3.005 1.366 3.005 4z"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="mailto:info@bitsandbytesassociation.ca" title="Email us">
+                        <img class="navbar-nav-svg" src="./assets/img/icons/email.png" alt="Email">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://twitter.com/rrcbba" target="_blank" rel="noopener" aria-label="Twitter">
+                        <svg class="navbar-nav-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 416.32" focusable="false">
+                            <title>Follow us on Twitter</title>
+                            <path d="M160.83 416.32c193.2 0 298.92-160.22 298.92-298.92 0-4.51 0-9-.2-13.52A214 214 0 0 0 512 49.38a212.93 212.93 0 0 1-60.44 16.6 105.7 105.7 0 0 0 46.3-58.19 209 209 0 0 1-66.79 25.37 105.09 105.09 0 0 0-181.73 71.91 116.12 116.12 0 0 0 2.66 24c-87.28-4.3-164.73-46.3-216.56-109.82A105.48 105.48 0 0 0 68 159.6a106.27 106.27 0 0 1-47.53-13.11v1.43a105.28 105.28 0 0 0 84.21 103.06 105.67 105.67 0 0 1-47.33 1.84 105.06 105.06 0 0 0 98.14 72.94A210.72 210.72 0 0 1 25 370.84a202.17 202.17 0 0 1-25-1.43 298.85 298.85 0 0 0 160.83 46.92" fill="currentColor"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://www.facebook.com/groups/865544533563505/" title="Find us on Facebook">
+                        <img class="navbar-nav-svg" src="./assets/img/logos/facebook.png" alt="Find us on Facebook">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://www.facebook.com/rrcbitsbytes/" title="Find us on Facebook">
+                        <img class="navbar-nav-svg" src="./assets/img/icons/facebookpage.png" alt="Find us on Facebook">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://www.instagram.com/rrcbba/" title="Follow us on Instagram">
+                        <img class="navbar-nav-svg" src="./assets/img/logos/instagram.png" alt="Follow us on Instagram">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2" href="https://www.linkedin.com/groups/13502653" title ="Connect with us on LinkedIn">
+                        <img class="navbar-nav-svg" src="./assets/img/logos/linkedin.png" alt="Connect with us on LinkedIn">
+                    </a>
+                </li>
+            </ul>
+  </div>
+  <div class="card-body copyright-text">
+    <p class="card-text">BBA &copy; <?=date('Y')?></p>
+  </div>
+</div>
+</footer>
--- index.php
+++ index.php
@@ -4,23 +4,23 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="./assets/img/icons/favicon.ico" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/main.css">
     <title>Hello, world!</title>
 </head>
 <body>
-<header>
+
 <?php
     include("header.php");
 ?>
-</header>
+
     <h1>Hello, world!</h1>
-<footer>
+
 <?php
     include("footer.php");
 ?>
-</footer>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="./assets/js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
